068750b67c7b6408be854f8fc4f6e16136fcc100
qasim-dev-12 <staytopdev@gmail.com>
2026-09-02 00:33:56 -0700

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,48 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18420530559"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-18420530559');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(){
+                  document.addEventListener('click', function(e){
+                  if(e.target.closest('a[href^="tel:"]')){
+                      gtag('event', 'conversion', {'send_to': 'AW-18420530559/_klSCO3JhewcEP_6y89E'});
+                      CG.conversion();
+                      initCCConvertion();
+                  }
+                  });
+              })();
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              document.addEventListener('click', function(e){
+                if(e.target.closest('a[href^="https://wa.me/"]')){
+                  gtag('event', 'conversion', {'send_to': 'AW-18420530559/IzxFCN24-uscEP_6y89E'});
+                  CG.conversion();
+                  initCCConvertion();
+                }
+              });
+            `,
+          }}
+        />
+      </head>
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${poppins.className}`}>
         <div className="isolate">
